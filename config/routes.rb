2848e78c1d8bee65f9ce755204b89de81a2aa926
shingo tanaka <stenstenktensten@gmail.com>
2020-03-07 06:40:11 +0000

    [Finish] favorite-comment

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  root 'home#top'
+  get 'home/about'
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
+    resource :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+end
