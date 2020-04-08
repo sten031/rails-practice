31dbd3cd48c5baa5c854aef1566828743e3d5f78
shingo tanaka <stenstenktensten@gmail.com>
2020-04-08 11:34:00 +0000

    firstcommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  get 'home/about'
   devise_for :users
+
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'follows' => 'relationships#follower', as: 'follows'
     get 'followers' => 'relationships#followed', as: 'followers'
   end
-  root 'home#top'
-  get 'home/about'
+
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
     resource :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_082822) do
+ActiveRecord::Schema.define(version: 2020_03_17_062125) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
